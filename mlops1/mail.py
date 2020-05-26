c04ffcb0dc4399c84ceb812190f70825521bd457
KanikaSharma1999 <63678107+KanikaSharma1999@users.noreply.github.com>
2020-05-27 00:12:05 +0530

    Update mail.py

--- mlops1/mail.py
+++ mlops1/mail.py
@@ -4,7 +4,7 @@ s = smtplib.SMTP('smtp.gmail.com', 587)
 # start TLS for security 
 s.starttls()   
 # Authentication 
-s.login("meetkanikasharma01011999@gmail.com", "#kanikasharma2276#") 
+s.login("sender's email", "pass") 
 # message to be sent 
 message = "Hey Developer, Finally we got the model trained. "
 # sending the mail 
